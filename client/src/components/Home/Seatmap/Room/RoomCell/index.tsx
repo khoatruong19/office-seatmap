5ebc5e7d273040c26715167646b0f56954d31fba
khoa-truong <khoa-truong@cybozu.vn>
2023-12-04 17:53:26 +0700

    working on cell map

--- client/src/components/Home/Seatmap/Room/RoomCell/index.tsx
+++ client/src/components/Home/Seatmap/Room/RoomCell/index.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import { cn } from "../../../../../lib/clsx";
 
 type Props = {
   className?: string;
 };
 
 const RoomCell = ({ className = "" }: Props) => {
-  return <div>RoomCell</div>;
+  return (
+    <div key={Math.random() * 1} className="relative h-12 w-12  z-50">
+      <div
+        className={cn(
+          "absolute top-0 left-0 w-[124%] h-[100%] bg-secondary",
+          className
+        )}
+      ></div>
+    </div>
+  );
 };
 
 export default RoomCell;
